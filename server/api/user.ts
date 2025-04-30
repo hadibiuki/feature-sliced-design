2dc6005669128d78998e5a0b4f11ae6a8bd17de6
hadibiuki <hadibiuki@gmail.com>
2025-04-30 12:32:32 +0330

    add use fetch

---
+++ server/api/user.ts
@@ -0,0 +1,8 @@
+export default defineEventHandler(() => {
+  return {
+    id: 1,
+    name: 'Sarah Biuki',
+    bio: 'Senior Nuxt Developer',
+    avatarUrl: 'https://i.pravatar.cc/150?img=7',
+  }
+})
